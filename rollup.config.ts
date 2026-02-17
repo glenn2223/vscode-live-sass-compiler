de0213a5d6211c1a7737a0a6956eedef763dd7c1
glen2223 <33450392+glenn2223@users.noreply.github.com>
2026-02-17 01:12:10 +0000

    Matched rollup to npm command and added helper message for CI checks (if they're ever needed)

--- rollup.config.ts
+++ rollup.config.ts
@@ -4,11 +4,10 @@ import typescript from "@rollup/plugin-typescript";
 import terser from "@rollup/plugin-terser";
 import json from "@rollup/plugin-json";
 import commonjs from "@rollup/plugin-commonjs";
-import { argv } from "process";
 
-const isTest = argv.some(
-    (arg) => arg === "TESTING" || arg.startsWith("TESTING="),
-);
+const isTest = process.env.TESTING !== undefined;
+
+console.log(`Building for ${isTest ? "testing" : "production"}...`);
 
 export default {
     input: "src/extension.ts",
